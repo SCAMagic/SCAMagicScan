06b8e15f2dec31a1da5e0e5611cd7b9c36cb294e
hbdhack3r <51362701+SCAMagic@users.noreply.github.com>
2022-12-16 14:48:28 +0800

    Update getresult.py

--- reverse/getresult.py
+++ reverse/getresult.py
@@ -2,6 +2,26 @@ import requests,urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def get_result(domain,token):
+	if 'dnslog.cn' in domain:
+		flag=dnslog1(domain,token)
+	else:
+		flag=dnslog2(domain,token)
+	return flag
+def dnslog1(domain,cookie):
+	url='http://dnslog.cn/getrecords.php?t=0.0701635123201233'
+	headers={
+	"Cookie":cookie
+	}
+	try:
+		r=requests.get(url,headers=headers,timeout=5,verify=False)
+		if '[]'!=r.text:
+			return True
+		else:
+			return False
+	except:
+		print("[!!!]	dnslog.cn刷新dnslog失败")
+		return False
+def dnslog2(domain,token):
 	url='https://log.xn--9tr.com/get_results'
 	headers={'Content-type': 'application/x-www-form-urlencoded'}
 	body='domain='+domain+'&token='+token
@@ -12,5 +32,5 @@ def get_result(domain,token):
 		else:
 			return False
 	except:
-		print("[!!!]	刷新dnslog失败")
+		print("[!!!]	dns.1433.eu.org刷新dnslog失败")
 		return False
